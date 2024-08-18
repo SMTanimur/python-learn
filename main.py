467f50954ab587f866fd625acef02a882c8dd41f
SM Tanimur Rahman <smtanimurrahman404@gmail.com>
2024-08-18 07:47:32 +0600

    Refactor variable assignments and add float data type assignment for x

--- main.py
+++ main.py
@@ -3,16 +3,22 @@ print(x)
 print(y)
 print(z)
 
-x = str("Hello World")	#str	
-x = int(20)	##float	
-x = complex(1j)	#complex	
-x = list(("apple", "banana", "cherry"))	#list	
-x = tuple(("apple", "banana", "cherry"))	#tuple	
-x = range(6)	#range	
-x = dict(name="John", age=36)	#dict	
-x = set(("apple", "banana", "cherry"))	#set	
-x = frozenset(("apple", "banana", "cherry"))	#frozenset	
-x = bool(5)	#bool	
-x = bytes(5)	#bytes	
-x = bytearray(5)	#bytearray	
-x = memoryview(bytes(5))
+# x = str("Hello World")	#str	
+# x = int(20)	##float	
+# x = complex(1j)	#complex	
+# x = list(("apple", "banana", "cherry"))	#list	
+# x = tuple(("apple", "banana", "cherry"))	#tuple	
+# x = range(6)	#range	
+# x = dict(name="John", age=36)	#dict	
+# x = set(("apple", "banana", "cherry"))	#set	
+# x = frozenset(("apple", "banana", "cherry"))	#frozenset	
+# x = bool(5)	#bool	
+# x = bytes(5)	#bytes	
+# x = bytearray(5)	#bytearray	
+# x = memoryview(bytes(5))
+
+
+# x = float(1)     # x will be 1.0
+# y = float(2.8)   # y will be 2.8
+# z = float("3")   # z will be 3.0
+# w = float("4.2") # w will be 4.2
