4a1d3c4fe79d5fed1d852c03d443e022e4bff1a6
SM Tanimur Rahman <smtanimurrahman404@gmail.com>
2024-08-18 07:46:04 +0600

    chore: Add data type assignments for x variable

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ x, y, z = "Orange", "Banana", "Cherry"
 print(x)
 print(y)
 print(z)
+
+x = str("Hello World")	#str	
+x = int(20)	##float	
+x = complex(1j)	#complex	
+x = list(("apple", "banana", "cherry"))	#list	
+x = tuple(("apple", "banana", "cherry"))	#tuple	
+x = range(6)	#range	
+x = dict(name="John", age=36)	#dict	
+x = set(("apple", "banana", "cherry"))	#set	
+x = frozenset(("apple", "banana", "cherry"))	#frozenset	
+x = bool(5)	#bool	
+x = bytes(5)	#bytes	
+x = bytearray(5)	#bytearray	
+x = memoryview(bytes(5))
