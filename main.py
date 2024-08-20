f4dd191790156a4628eab53d9416ebde5c4682be
SM Tanimur Rahman <smtanimurrahman404@gmail.com>
2024-08-20 20:12:37 +0600

    chore: Refactor variable assignments and add print statements

--- main.py
+++ main.py
@@ -21,4 +21,30 @@ print(z)
 # x = float(1)     # x will be 1.0
 # y = float(2.8)   # y will be 2.8
 # z = float("3")   # z will be 3.0
-# w = float("4.2") # w will be 4.2
+w = float("4.2") # w will be 4.2
+
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit, hffdhdfhdfh fhdfh
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
+
+b = "Hello, World!"
+print(b[2:5])
+
+b = "Hello, World!"
+print(b[:5])
+
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
